ebcc9fd8f58c1a8dd3c94ed0b5f408043df0767f
Vincenzo Greco <grecovincenzo98@gmail.com>
2023-03-21 18:09:52 +0100

    Assignments 06 and 07

---
+++ A06/WorldView.hpp
@@ -0,0 +1,38 @@
+
+glm::mat4 MakeViewProjectionMatrix(float Ar, float Alpha, float Beta, float Rho, glm::vec3 Pos) {
+    // Creates a view projection matrix, with near plane at 0.1, and far plane at 50.0, and
+    // aspect ratio given in <Ar>. The view matrix, uses the Look-in-Direction model, with
+    // vector <pos> specifying the position of the camera, and angles <Alpha>, <Beta> and <Rho>
+    // defining its direction. In particular, <Alpha> defines the direction (Yaw), <Beta> the
+    // elevation (Pitch), and <Rho> the roll.
+
+    float near = 0.1f;
+    float far = 50.0f;
+
+    glm::mat4 P = glm::perspective(glm::radians(45.0f), Ar, near, far);
+    P[1][1] *= -1;
+
+    glm::mat4 V =
+            glm::rotate(glm::mat4(1.0), -Rho, glm::vec3(0, 0, 1)) *
+            glm::rotate(glm::mat4(1.0), -Beta, glm::vec3(1, 0, 0)) *
+            glm::rotate(glm::mat4(1.0), -Alpha, glm::vec3(0, 1, 0)) *
+            glm::translate(glm::mat4(1.0), -Pos);
+
+    glm::mat4 M = P * V;
+
+    return M;
+}
+
+glm::mat4 MakeWorldMatrix(glm::vec3 pos, glm::quat rQ, glm::vec3 size) {
+    // creates and returns a World Matrix that positions the object at <pos>,
+    // orients it according to <rQ>, and scales it according to the sizes
+    // given in vector <size>
+
+    glm::mat4 T = glm::translate(glm::mat4(1), pos);
+    glm::mat4 R = glm::mat4(rQ);
+    glm::mat4 S = glm::scale(glm::mat4(1), size);
+
+    glm::mat4 M = T * R * S;
+
+    return M;
+}
