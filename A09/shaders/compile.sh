590fb679e131a9eeb3443bdcca75c360d06700f6
Vincenzo Greco <grecovincenzo98@gmail.com>
2023-11-24 19:17:49 +0100

    Assignment 9 default compilation on v2 shader

--- A09/shaders/compile.sh
+++ A09/shaders/compile.sh
@@ -4,9 +4,9 @@ rm -f PhongFrag.spv
 rm -f PhongVert.spv
 
 if [ "$1" = "2" ]; then
-  glslc PhongShader.v2.frag -o PhongFrag.spv
-  glslc PhongShader.v2.vert -o PhongVert.spv
-else
   glslc PhongShader.frag -o PhongFrag.spv
   glslc PhongShader.vert -o PhongVert.spv
+else
+  glslc PhongShader.v2.frag -o PhongFrag.spv
+  glslc PhongShader.v2.vert -o PhongVert.spv
 fi
