becf62f75dc38d6a761f24c05a6c0b169145b4fc
MarcoRomanini <marco.romanini.99@gmail.com>
2023-12-12 22:55:57 +0100

    Camera position and movement fixed

--- SandroRun/main.cpp
+++ SandroRun/main.cpp
@@ -74,11 +74,11 @@ protected:
     MeshUniformBlock uboRoad;
 
     void setWindowParameters() {
-        windowWidth = 800;
-        windowHeight = 600;
-        windowTitle = "A16";
+        windowWidth = 1280;
+        windowHeight = 720;
+        windowTitle = "Sandro Run";
         windowResizable = GLFW_TRUE;
-        initialBackgroundColor = {0.0f, 0.005f, 0.01f, 1.0f};
+        initialBackgroundColor = {0.0f, 1.0f, 1.0f, 1.0f};
 
         uniformBlocksInPool = 3;
         texturesInPool = 1;
@@ -197,10 +197,9 @@ protected:
         }
 
         glm::mat4 ViewProj;
-        glm::vec3 camPos;
         glm::mat4 World;
 
-        cameraPosition(ViewProj, World, camPos);
+        updateCameraPosition(ViewProj, World);
 
         gubo.DlightDir = glm::normalize(glm::vec3(1.0f, 2.0f, 3.0f));
         gubo.DlightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
@@ -217,31 +216,31 @@ protected:
         DSMoto.map(currentImage, &uboMoto, sizeof(uboMoto), 0);
 
 
-//        World = glm::mat4(1.0f);
-//        uboRoad.amb = 1.0f;
-//        uboRoad.gamma = 180.0f;
-//        uboRoad.sColor = glm::vec3(1.0f);
-//        uboRoad.mvpMat = ViewProj * World;
-//        uboRoad.mMat = World;
-//        uboRoad.nMat = glm::inverse(glm::transpose(World));
-//        DSRoad.map(currentImage, &uboRoad, sizeof(uboRoad), 0);
+       World = glm::mat4(1.0f);
+       uboRoad.amb = 1.0f;
+       uboRoad.gamma = 180.0f;
+       uboRoad.sColor = glm::vec3(1.0f);
+       uboRoad.mvpMat = ViewProj * World;
+       uboRoad.mMat = World;
+       uboRoad.nMat = glm::inverse(glm::transpose(World));
+       DSRoad.map(currentImage, &uboRoad, sizeof(uboRoad), 0);
     }
 
-    void cameraPosition(glm::mat4 &ViewProj, glm::mat4 &World, glm::vec3 &camPos) {
-        const float FOVy = glm::radians(90.0f);
-        const float nearPlane = 0.1f;
-        const float farPlane = 100.0f;
+    void updateCameraPosition(glm::mat4 &ViewProj, glm::mat4 &World) {
+        const float FOV_Y = glm::radians(45.0f);
+        const float NEAR_PLANE = 0.1f;
+        const float FAR_PLANE = 100.0f;
 
-        const glm::vec3 startingPos = glm::vec3(0.0f, 0.0f, 0.0f);
+        const glm::vec3 STARTING_POS = glm::vec3(0.0f, 0.0f, 0.0f);
 
-        const float camHeight = 0.25f;
-        const float camDist = 1.5f;
+        const float CAM_HEIGHT = 0.75f;
+        const float CAM_DIST = 3.5f;
 
-        const float minPitch = glm::radians(-8.75f);
-        const float maxPitch = glm::radians(60.0f);
+        const float MIN_PITCH = glm::radians(-30.0f);
+        const float MAX_PITCH = glm::radians(80.0f);
 
-        const float rotSpeed = glm::radians(90.0f);
-        const float movSpeed = 1.0f;
+        const float ROT_SPEED = glm::radians(120.0f);
+        const float MOV_SPEED = 3.0f;
 
         float deltaT;
         glm::vec3 m = glm::vec3(0.0f), r = glm::vec3(0.0f);
@@ -249,50 +248,50 @@ protected:
         bool fire = false;
         getSixAxis(deltaT, m, r, fire);
 
-        static bool wasFire = false;
-        bool handleFire = (wasFire && (!fire));
-        wasFire = fire;
+        // static bool wasFire = false;
+        // bool handleFire = (wasFire && (!fire));
+        // wasFire = fire;
 
-        static glm::vec3 pos = startingPos;
+        static glm::vec3 pos = STARTING_POS;
         static float yaw = 0.0f, pitch = 0.0f, roll = 0.0f;
         static float yawNew = 0.0f, pitchNew = 0.0f, rollNew = 0.0f;
 
-        const float lambda = 10.0f;
+        const float LAMBDA = 10.0f;
 
-        static glm::vec3 newCameraPos;
+        static glm::vec3 cameraPosition, newCameraPosition;
         glm::vec3 a;
 
         glm::vec3 ux = glm::vec3(glm::rotate(glm::mat4(1), -yaw, glm::vec3(0, 1, 0)) * glm::vec4(1, 0, 0, 1));
         glm::vec3 uy = glm::vec3(0, 1, 0);
         glm::vec3 uz = glm::vec3(glm::rotate(glm::mat4(1), -yaw, glm::vec3(0, 1, 0)) * glm::vec4(0, 0, -1, 1));
 
-        pitchNew += rotSpeed * r.x * deltaT;
-        pitchNew = glm::clamp(pitchNew, minPitch, maxPitch);
-        pitch = pitch * glm::exp(-lambda * deltaT) + pitchNew * (1 - glm::exp(-lambda * deltaT)); // Pitch damping
-        pitch = glm::clamp(pitch, minPitch, maxPitch);
+        pitchNew += ROT_SPEED * r.x * deltaT;
+        pitchNew = glm::clamp(pitchNew, MIN_PITCH, MAX_PITCH);
+        pitch = pitch * glm::exp(-LAMBDA * deltaT) + pitchNew * (1 - glm::exp(-LAMBDA * deltaT)); // Pitch damping
+        pitch = glm::clamp(pitch, MIN_PITCH, MAX_PITCH);
 
-        yawNew += rotSpeed * r.y * deltaT;
-        yaw = yaw * glm::exp(-lambda * deltaT) + yawNew * (1 - glm::exp(-lambda * deltaT)); // Yaw damping
+        yawNew += ROT_SPEED * r.y * deltaT;
+        yaw = yaw * glm::exp(-LAMBDA * deltaT) + yawNew * (1 - glm::exp(-LAMBDA * deltaT)); // Yaw damping
 
-        rollNew += rotSpeed * r.z * deltaT;
-        roll = roll * glm::exp(-lambda * deltaT) + rollNew * (1 - glm::exp(-lambda * deltaT)); // Roll damping
+        rollNew += ROT_SPEED * r.z * deltaT;
+        roll = roll * glm::exp(-LAMBDA * deltaT) + rollNew * (1 - glm::exp(-LAMBDA * deltaT)); // Roll damping
 
-        pos += ux * movSpeed * m.x * deltaT;
-        pos += uz * movSpeed * m.z * deltaT;
-        pos += uy * movSpeed * m.y * deltaT;
+        pos += ux * MOV_SPEED * m.x * deltaT;
+        pos += uz * MOV_SPEED * m.z * deltaT;
+        pos += uy * MOV_SPEED * m.y * deltaT;
 
         World = glm::translate(glm::mat4(1), pos) *
                 glm::rotate(glm::mat4(1), -yaw, glm::vec3(0, 1, 0));
 
-        newCameraPos = World * glm::vec4(0, camHeight + camDist * glm::sin(pitch), camDist * glm::cos(pitch), 1);
+        newCameraPosition = World * glm::vec4(0, CAM_HEIGHT + CAM_DIST * glm::sin(pitch), CAM_DIST * glm::cos(pitch), 1);
 
-        a = World * glm::vec4(0, 0, 0, 1) + glm::vec4(0, camHeight, 0, 0);
+        a = World * glm::vec4(0, 0, 0, 1) + glm::vec4(0, CAM_HEIGHT, 0, 0);
 
-        camPos = camPos * glm::exp(-lambda * deltaT) +
-                 newCameraPos * (1 - glm::exp(-lambda * deltaT)); // Camera damping
+        cameraPosition = cameraPosition * glm::exp(-LAMBDA * deltaT) +
+                 newCameraPosition * (1 - glm::exp(-LAMBDA * deltaT)); // Camera damping
 
-        glm::mat4 View = glm::rotate(glm::mat4(1), -roll, glm::vec3(0, 0, 1)) * glm::lookAt(camPos, a, uy);
-        glm::mat4 Proj = glm::perspective(FOVy, Ar, nearPlane, farPlane);
+        glm::mat4 View = glm::rotate(glm::mat4(1), -roll, glm::vec3(0, 0, 1)) * glm::lookAt(cameraPosition, a, uy);
+        glm::mat4 Proj = glm::perspective(FOV_Y, Ar, NEAR_PLANE, FAR_PLANE);
         Proj[1][1] *= -1;
         ViewProj = Proj * View;
     }
